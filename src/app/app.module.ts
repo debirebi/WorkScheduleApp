ff23f7e2c5e0809305cbb56cbbc3eaaa988aaf8c
debirebi <debreczenyr@gmail.com>
2018-12-17 19:51:06 +0100

    Refactoring + Deployment
    Refactored a lot of code because production build failed. Deployed to FireBase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,35 +1,65 @@
 
 import { NgModule } from '@angular/core';
-import {CalendarModule, DateAdapter} from 'angular-calendar';
-import {CalendarHeaderComponent} from './calendar/calendar-header.component';
 import {CommonModule} from '@angular/common';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {adapterFactory} from 'angular-calendar/date-adapters/date-fns';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {CalComponent} from './calendar/calendar.component';
-import { NoteBoardComponent } from './note-board/note-board.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppRoutingModule} from './app-routing.module';
+import {HttpClientModule} from '@angular/common/http';
+import {CalendarModule, DateAdapter} from 'angular-calendar';
+import {adapterFactory} from 'angular-calendar/date-adapters/date-fns';
+
+import {AppComponent} from './app.component';
+import {LoginFormComponent} from './login-form/login-form.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import { HeaderComponent} from './header/header.component';
 import {
   MatButtonModule,
   MatCardModule,
-  MatExpansionModule,
-  MatFormFieldModule,
-  MatInputModule,
-  MatListModule,
-  MatMenuModule, MatSnackBarModule
+  MatDividerModule,
+  MatFormFieldModule, MatGridListModule,
+  MatInputModule, MatListModule, MatMenuModule, MatSidenavModule, MatSnackBarModule,
 } from '@angular/material';
+import {NoteBoardComponent} from './note-board/note-board.component';
+import {CalendarHeaderComponent} from './calendar/calendar-header.component';
+import {CalComponent} from './calendar/calendar.component';
 
 @NgModule({
+  declarations: [HeaderComponent,
+    AppComponent,
+    LoginFormComponent,
+    DashboardComponent,
+    NoteBoardComponent,
+    CalendarHeaderComponent,
+    CalComponent],
   imports: [BrowserAnimationsModule, MatCardModule, CommonModule,
     FormsModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule,
     BrowserAnimationsModule,
     MatButtonModule,
     MatListModule,
     MatMenuModule,
+    MatListModule,
+    MatGridListModule,
+    BrowserModule,
+    MatMenuModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MatCardModule,
+    MatFormFieldModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatSnackBarModule,
+    MatInputModule,
+    MatDividerModule,
+    MatSidenavModule,
     CalendarModule.forRoot({
-    provide: DateAdapter,
-    useFactory: adapterFactory
-  }), CalendarModule],
-  declarations: [CalendarHeaderComponent, CalComponent],
-  exports: [CalendarHeaderComponent, CalComponent]
+      useFactory: adapterFactory,
+      provide: DateAdapter
+    })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,68 +1,14 @@
-import {enableProdMode, NgModule} from '@angular/core';
+import {enableProdMode} from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { environment } from './environments/environment';
-import {BrowserModule} from '@angular/platform-browser';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {CalendarModule, DateAdapter} from 'angular-calendar';
-import {adapterFactory} from 'angular-calendar/date-adapters/date-fns';
 import {AppModule} from './app/app.module';
-import {AppComponent} from './app/app.component';
-import {LoginFormComponent} from './app/login-form/login-form.component';
-import {DashboardComponent} from './app/dashboard/dashboard.component';
-import {AppRoutingModule} from './app/app-routing.module';
-import { HeaderComponent} from './app/header/header.component';
-import {
-  MatButtonModule,
-  MatCardModule,
-  MatDividerModule,
-  MatFormFieldModule, MatGridListModule,
-  MatInputModule, MatListModule, MatSidenavModule,
-} from '@angular/material';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import 'hammerjs';
-import {NoteBoardComponent} from './app/note-board/note-board.component';
-import {HttpClientModule} from '@angular/common/http';
-
 
 
 if (environment.production) {
   enableProdMode();
 }
-
-@NgModule({
-  declarations: [
-    HeaderComponent,
-    AppComponent,
-    LoginFormComponent,
-    DashboardComponent,
-    NoteBoardComponent
-  ],
-  imports: [
-    MatListModule,
-    MatGridListModule,
-    BrowserModule,
-    BrowserAnimationsModule,
-    AppModule,
-    AppRoutingModule,
-    MatCardModule,
-    MatFormFieldModule,
-    FormsModule,
-    HttpClientModule,
-    ReactiveFormsModule,
-    MatButtonModule,
-    MatInputModule,
-    MatDividerModule,
-    MatSidenavModule,
-    CalendarModule.forRoot({
-      useFactory: adapterFactory,
-      provide: DateAdapter
-    })
-  ],
-  bootstrap: [AppComponent]
-})
-export class BootstrapModule {}
-
-platformBrowserDynamic().bootstrapModule(BootstrapModule).then(ref => {
+platformBrowserDynamic().bootstrapModule(AppModule).then(ref => {
   // Ensure Angular destroys itself on hot reloads.
   if (window['ngRef']) {
     window['ngRef'].destroy();
